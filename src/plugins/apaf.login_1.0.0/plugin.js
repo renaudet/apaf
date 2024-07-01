ea375e363664b8fba292ef07a3202efcfb8cf42a
Nicolas <Nicolas@MOEBIUS>
2024-07-01 12:18:58 +0200

    Implements a safety guard based on an IP cache to prevent brute-force password discovery

--- src/plugins/apaf.login_1.0.0/plugin.js
+++ src/plugins/apaf.login_1.0.0/plugin.js
@@ -9,6 +9,7 @@ const moment = require('moment');
 const DATATYPE_PLUGIN_ID = 'apaf.datatype';
 const SECURITY_SERVICE_NAME = 'apaf-security';
 const USER_DATATYPE = 'user';
+const CACHE_EVICTION_LOOP_TIMEOUT = 60*1000;
 
 var plugin = new ApafPlugin();
 plugin.available = false;
@@ -20,6 +21,7 @@ plugin.beforeExtensionPlugged = function(){
 
 plugin.checkPreconditions = function(){
 	this.debug('->checkPreconditions()');
+	this.checkCacheLoop();
 	let datatypePlugin = this.runtime.getPlugin(DATATYPE_PLUGIN_ID);
 	datatypePlugin.query(USER_DATATYPE,{"selector": {"login": {"$eq": "admin"}}},function(err,data){
 		if(err){
@@ -55,6 +57,23 @@ plugin.checkPreconditions = function(){
 			}
 		}
 	});
+}
+
+plugin.checkCacheLoop = function(){
+	let cacheDataToRemove = [];
+	let now = moment();
+	for(var ip in this.ipCache){
+		let cacheData = this.ipCache[ip];
+		if(now.diff(cacheData.lastAttempt)>3*CACHE_EVICTION_LOOP_TIMEOUT){
+			cacheDataToRemove.push(ip);
+		}
+	}
+	for(var i=0;i<cacheDataToRemove.length;i++){
+		let ip = cacheDataToRemove[i];
+		this.debug('deleting evicted IP data from the cache: '+JSON.stringify(this.ipCache[ip]));
+		delete this.ipCache[ip];
+	}
+	setTimeout(function(){ plugin.checkCacheLoop(); },CACHE_EVICTION_LOOP_TIMEOUT);
 }
 
 plugin.authenticate = function(userId,password,then){
@@ -100,53 +119,84 @@ plugin.authenticate = function(userId,password,then){
 	}
 }
 
+plugin.ipCache = {};
+
 plugin.loginHandler = function(req,res){
 	plugin.debug('->loginHandler()');
 	res.set('Content-Type','application/json');
 	if(plugin.available){
+		let userIp = req.ip;
+		console.log('User IP: '+userIp);
 		let userId = req.body.userid;
 		let password = req.body.passwd;
 		let md5Pwd = md5(password);
 		if(userId && userId.length>0 && password && password.length>=6){
-			let datatypePlugin = plugin.runtime.getPlugin(DATATYPE_PLUGIN_ID);
-			datatypePlugin.query(USER_DATATYPE,{"selector": {"login": {"$eq": userId}}},function(err,data){
-				if(err){
-					plugin.debug('<-loginHandler(500)');
-					let errorMsg = 'unable to retrieve user "'+userId+'"';
-					res.json({"status": 500,"message": "@apaf.page.login.error.internal","data": errorMsg});
-				}else{
-					if(data && data.length==0){
-						plugin.debug('<-loginHandler(404)');
-						res.json({"status": 404,"message": "@apaf.page.login.error.unauthorized"});
+			//get previous login attempt from cache
+			let userConnectionData = plugin.ipCache[userIp];
+			let tooManyLoginAttempts = false;
+			if(userConnectionData){
+				if(userConnectionData.attemptCount>2){
+					tooManyLoginAttempts = true;
+					userConnectionData.lastAttempt = moment();
+				}
+			}else{
+				let lastAttempt = moment();
+				plugin.ipCache[userIp] = {"attemptCount": 0,"lastAttempt": lastAttempt };
+				userConnectionData = plugin.ipCache[userIp];
+			}
+			if(!tooManyLoginAttempts){
+				let datatypePlugin = plugin.runtime.getPlugin(DATATYPE_PLUGIN_ID);
+				datatypePlugin.query(USER_DATATYPE,{"selector": {"login": {"$eq": userId}}},function(err,data){
+					if(err){
+						plugin.debug('<-loginHandler(500)');
+						let errorMsg = 'unable to retrieve user "'+userId+'"';
+						res.json({"status": 500,"message": "@apaf.page.login.error.internal","data": errorMsg});
 					}else{
-						let registeredUser = data[0];
-						if(registeredUser.password==md5Pwd){
-							plugin.debug('<-loginHandler(200)');
-							let securityEngine = plugin.getService(SECURITY_SERVICE_NAME);
-							securityEngine.loadUserRoles(registeredUser,function(user){
-								plugin.debug('user:');
-								plugin.debug(JSON.stringify(user,null,'\t'));
-								req.session.user = user;
-								req.session.user.password = password;
-								let now = moment();
-								req.session.lastAccess = now;
-								req.session.created = now;
-								req.session.alive = true;
-								res.json({"status": 200,"message": "ok","data": user.roles});
-							});
+						if(data && data.length==0){
+							lastAttempt = moment();
+							userConnectionData.lastAttempt = lastAttempt;
+							userConnectionData.attemptCount++;
+							plugin.debug('<-loginHandler(404)');
+							//res.json({"status": 404,"message": "@apaf.page.login.error.unauthorized"});
+							setTimeout(function(){ res.json({"status": 404,"message": "@apaf.page.login.error.unauthorized"}); },3000);
 						}else{
-							plugin.debug('<-loginHandler(401)');
-							res.json({"status": 401,"message": "@apaf.page.login.error.invalid"});
+							let registeredUser = data[0];
+							if(registeredUser.password==md5Pwd){
+								userConnectionData.attemptCount = 0;
+								plugin.debug('<-loginHandler(200)');
+								let securityEngine = plugin.getService(SECURITY_SERVICE_NAME);
+								securityEngine.loadUserRoles(registeredUser,function(user){
+									plugin.debug('user:');
+									plugin.debug(JSON.stringify(user,null,'\t'));
+									req.session.user = user;
+									req.session.user.password = password;
+									let now = moment();
+									req.session.lastAccess = now;
+									req.session.created = now;
+									req.session.alive = true;
+									res.json({"status": 200,"message": "ok","data": user.roles});
+								});
+							}else{
+								lastAttempt = moment();
+								userConnectionData.lastAttempt = lastAttempt;
+								userConnectionData.attemptCount++;
+								plugin.debug('<-loginHandler(401)');
+								//res.json({"status": 401,"message": "@apaf.page.login.error.invalid"});
+								setTimeout(function(){ res.json({"status": 401,"message": "@apaf.page.login.error.invalid"}); },5000);
+							}
 						}
 					}
-				}
-			});
+				});
+			}else{
+				plugin.debug('<-loginHandler(423)');
+				setTimeout(function(){ res.json({"status": 423,"message": "@apaf.page.login.error.tooMany"}); },userConnectionData.attemptCount*10000);
+			}
 		}else{
 			plugin.debug('<-loginHandler(412)');
 			res.json({"status": 412,"message": "@apaf.page.login.error.invalid"});
 		}
 	}else{
-		plugin.debug('<-loginHandler(412)');
+		plugin.debug('<-loginHandler(425)');
 		res.json({"status": 425,"message": "@apaf.page.login.error.unavailable"});
 	}
 }
